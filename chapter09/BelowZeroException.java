68150808b2ac27ddaf5c3f330a5aeaba1ef35a16
Ron Lee <3475215040@qq.com>
2023-08-24 21:20:35 +0800

    将BelowZeroException变为非运行时异常（必检异常），巩固try-catch对必检异常的处理

--- chapter09/BelowZeroException.java
+++ chapter09/BelowZeroException.java
@@ -1,6 +1,6 @@
 package chapter09;
 
-public class BelowZeroException extends RuntimeException{
+public class BelowZeroException extends Exception{
     // 自定义异常，输入负数弹出此错误
     static final long serialVersionUID = -7034897L;
 
