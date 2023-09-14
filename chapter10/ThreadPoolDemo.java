9d838185d479244d01c906266b7033ddd8cd6b5f
Ron Lee <3475215040@qq.com>
2023-09-14 20:20:54 +0800

    线程的简单了解与使用

---
+++ chapter10/ThreadPoolDemo.java
@@ -0,0 +1,39 @@
+package chapter10;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * 线程池的简单了解
+*/
+
+public class ThreadPoolDemo {
+    public static void main(String[] args) {
+        // TODO 线程安全的懒汉式实现，之后要补充
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        ThreadPoolExecutor service1 = (ThreadPoolExecutor) service;
+
+        // 设置线程池中线程数的上限
+        service1.setMaximumPoolSize(50);
+
+        service.execute(new Runnable() {
+            @Override
+            public void run(){
+                for (int i = 0; i <= 100; i += 2) {
+                    System.out.println(Thread.currentThread().getName() + ": " + i);
+                }
+            }
+        });
+
+        try {
+            Future<Integer> future = service.submit(new NumCallable());
+            System.out.println("总和为：" + future.get());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        service.shutdown();
+    }
+}
