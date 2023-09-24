b47d5e3237f87bd56e50fb1eb4560e63316f6a14
Ron Lee <3475215040@qq.com>
2023-09-24 19:31:15 +0800

    SimpleDateFormat类的使用

---
+++ chapter10/SDFDemo.java
@@ -0,0 +1,20 @@
+package chapter10;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SDFDemo {
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS E Z");
+        Date date = new Date();
+        // Date -> String
+        String dateStr = sdf.format(date);
+        System.out.println(dateStr);
+
+        // String -> Date
+        Date date2 = sdf.parse(dateStr);
+        System.out.println(date2);
+
+    }
+}
