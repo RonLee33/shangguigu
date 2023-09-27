4518cccd9418493e0562e1150b91d43531374948
Ron Lee <3475215040@qq.com>
2023-09-27 19:29:48 +0800

    Compareable的学习，Comparable未实现时，会报异常

---
+++ chapter11/comparepack/CompareableDemo.java
@@ -0,0 +1,35 @@
+package chapter11.comparepack;
+
+import java.util.Arrays;
+
+public class CompareableDemo{
+    public static void main(String[] args) {
+        String[] names = {"A", "C", "F", "T", "B", "G", "D", "E"};
+        
+        // 因为String实现了Compareable,所以才能sort()
+        Arrays.sort(names);
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+
+        Person[] persons = {
+            new Person("James Smith", 20),
+            new Person("Emily Johnson", 26),
+            new Person("William Brown", 19),
+            new Person("Olivia Taylor", 48),
+            new Person("Thomas Davis", 35),
+            new Person("Ava Wilson", 51),
+            new Person("George Jones", 24),
+            new Person("Isabella Clark", 27),
+            new Person("Henry Thompson", 15),
+            new Person("Sophia Evans", 31)
+        };
+
+        // java.lang.ClassCastException: class chapter11.comparepack.Person cannot be cast to class java.lang.Comparable
+        Arrays.sort(persons);
+
+        for (Person person : persons) {
+            System.out.println(person);
+        }
+    }
+}
