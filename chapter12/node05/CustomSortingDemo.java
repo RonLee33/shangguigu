0f605cdfb2ae517b15a04dd49c858d0f8a10d5cc
Ron Lee <3475215040@qq.com>
2023-10-21 19:12:31 +0800

    TreeSet的定制排序(Comparator)

--- chapter12/node05/CustomSortingDemo.java
+++ chapter12/node05/CustomSortingDemo.java
@@ -1,5 +1,6 @@
 package chapter12.node05;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.TreeSet;
@@ -27,14 +28,30 @@ public class CustomSortingDemo {
 
         System.out.println(set.contains(new User(8, "Jack", 23))); //true
 
-        System.out.println("set中的元素排序后：");
+        System.out.println("set中的元素排序(按照age从小到大的顺序排列，如果age相同，则按照name从大到小的顺序排列)后：");
         TreeSet<User> treeSet = new TreeSet<User>(set);
         Iterator<User> iterator1 = treeSet.iterator();
         while(iterator1.hasNext()){
             System.out.println(iterator1.next());
         }
 
-        
+        // Comparator匿名类实例comparator，定制覆盖User compareTo()中原有的排序规则
+        Comparator<User> comparator = new Comparator<User>() {
+            @Override
+            public int compare(User u1, User u2){
+                // 按照User的姓名的从小到大的顺序排列
+                return u1.getName().compareTo(u2.getName());
+            }
+        };
+
+        System.out.println("按照User的姓名的从小到大的顺序排列:");
+        TreeSet<User> treeSet1 = new TreeSet<User>(comparator);
+        treeSet1.addAll(set);
+        Iterator<User> iterator2 = treeSet1.iterator();
+        while(iterator2.hasNext()){
+            System.out.println(iterator2.next());
+        }
+
     }
 }
 
