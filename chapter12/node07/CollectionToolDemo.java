61c10a61e9d1a82d5098222b2886ea4d90ca7790
Ron Lee <3475215040@qq.com>
2023-10-28 23:06:30 +0800

    Collections常用静态工具方法(2)

--- chapter12/node07/CollectionToolDemo.java
+++ chapter12/node07/CollectionToolDemo.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
+import java.util.List;
 
 public class CollectionToolDemo {
     public static void main(String[] args) {
@@ -56,7 +57,37 @@ public class CollectionToolDemo {
         }
 
         // 3.复制替换
+        ArrayList<Integer> destList = new ArrayList<Integer>();
+
+        for (int i = 0; i < list.size(); i++) {
+            destList.add(i);
+        }
+
+        // 目的集合的size必须 >= 源集合的size,注意：是size，不是initialCapacity
+        // 否则会报 java.lang.IndexOutOfBoundsException 的异常
+        Collections.copy(destList, list); 
+        System.out.println("list=" + list);
+        System.out.println("destList=" + destList);
+
+        Collections.replaceAll(list, 4, 999);
+        System.out.println("把list中所有的4替换为999后" + list);
+
+        /* 使list变为只读集合（无法修改的集合），
+        可放防止数据被修改， 同时为只读操作提供了更好的性能*/
+
+        List<Integer> onlyReadList = 
+            Collections.unmodifiableList(list);
         
+        // 以下操作会报 java.lang.UnsupportedOperationException 异常，只读集合不能进行写操作
+        try {
+            onlyReadList.add(1);
+        } catch (UnsupportedOperationException e) {
+            System.out.println("unmodifiableList()生成的只读集合，不允许写操作");
+        }
+        
+        // 将指定的元素加入到list中
+        Collections.addAll(list, 333, 334, 335, 336, 337, 338, 339, 340);
+        System.out.println("list=" + list);
 
     }
 }
